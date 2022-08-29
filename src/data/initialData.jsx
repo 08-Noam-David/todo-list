09b5e68c23ee96db8cec98c86025e9fe1f360801
Noam David <107786627+08-Noam-David@users.noreply.github.com>
2022-08-29 16:08:41 +0300

    Add initial data

---
+++ src/data/initialData.jsx
@@ -0,0 +1,22 @@
+export default [
+    {
+        name: 'Print bills',
+        isDone: false
+    },
+    {
+        name: 'Gym workout',
+        isDone: false
+    },
+    {
+        name: 'Organize next month calendar',
+        isDone: true
+    },
+    {
+        name: 'Clean bathroom',
+        isDone: false
+    },
+    {
+        name: 'Ship delivery to Spain',
+        isDone: true
+    }
+];
