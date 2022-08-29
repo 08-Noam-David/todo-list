68723a715c9cea0ca6c3610a8f911e3fb57b0b15
Noam David <107786627+08-Noam-David@users.noreply.github.com>
2022-08-29 15:44:21 +0300

    Remove router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Layout from "./pages/Layout";
 import MainPage from "./pages/MainPage";
-import AddTask from "./pages/AddTask";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index path="/" element={<MainPage />} />
-          <Route path="new" element={<AddTask />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <MainPage />
   );
 }
 
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export class MainPage extends Component {
   render() {
     return (
-      <div>MainPage</div>
-    )
+      <React.Fragment>
+        <header className="container-fluid bg-light">
+          <h1>Todo app</h1>
+        </header>
+        <main>
+          blah
+        </main>
+      </React.Fragment>
+    );
   }
 }
 
-export default MainPage
+export default MainPage;
