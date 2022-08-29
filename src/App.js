8bdfaf6b4b0567d7d700cc149f817201a2ccb559
Noam David <107786627+08-Noam-David@users.noreply.github.com>
2022-08-29 15:32:17 +0300

    Create routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Layout from "./pages/Layout";
+import MainPage from "./pages/MainPage";
+import AddTask from "./pages/AddTask";
+
 function App() {
   return (
-    <div className="App">
-      blah
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index path="/" element={<MainPage />} />
+          <Route path="new" element={<AddTask />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
