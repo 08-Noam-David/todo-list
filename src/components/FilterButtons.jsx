7013ecf8e0fbd1c5a21f5e556d0d577799db3bcd
Noam David <107786627+08-Noam-David@users.noreply.github.com>
2022-08-30 11:28:17 +0300

    Add FilterButtons

--- src/components/FilterButtons.jsx
+++ src/components/FilterButtons.jsx
@@ -1,11 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class FilterButtons extends Component {
   render() {
     return (
-      <div>FilterButtons</div>
-    )
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <button className="btn btn-primary">ALL</button>
+            <button className="btn btn-primary">COMPLETED</button>
+            <button className="btn btn-primary">LEFT</button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-export default FilterButtons
+export default FilterButtons;
