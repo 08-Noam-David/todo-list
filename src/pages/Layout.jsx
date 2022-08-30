b3b3bc328a4d2ea9d742ec0155fc88f3094c7717
Noam David <107786627+08-Noam-David@users.noreply.github.com>
2022-08-30 11:35:20 +0300

    Removed unncessary import

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import SiteHeader from '../components/SiteHeader';
 
