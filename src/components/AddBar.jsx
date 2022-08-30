023d65e7cbb11eb801203403fb3b67c733efe7da
Noam David <107786627+08-Noam-David@users.noreply.github.com>
2022-08-30 09:52:51 +0300

    Add AddBar

--- src/components/AddBar.jsx
+++ src/components/AddBar.jsx
@@ -1,11 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class AddBar extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   render() {
     return (
-      <div>AddBar</div>
-    )
+      <form onSubmit={this.handleSubmit}>
+        <div className="row mb-3">
+          <div className="col-sm-10">
+            <input
+              className="form-control"
+              type="text"
+              name="task"
+              placeholder="What do you need to do today?"
+            />
+          </div>
+          <div className="col-sm-2">
+            <button type="submit" className="btn btn-primary">
+              Add
+            </button>
+          </div>
+        </div>
+      </form>
+    );
   }
 }
 
-export default AddBar
+export default AddBar;
